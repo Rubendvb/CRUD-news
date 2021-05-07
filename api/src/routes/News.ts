59a0581c3ba71ee7f11d0a5000112f4d1ed38e26
Rubendvb <rubenvasquez556@gmail.com>
2021-05-07 20:53:53 -0300

    Estruturando arquivo News.ts

---
+++ api/src/routes/News.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+const newsSchema = new Schema(
+  {
+    title: {
+      type: String,
+      requerid: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    url: {
+      type: String,
+      requerid: true,
+      trim: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export default model("News", newsSchema);
