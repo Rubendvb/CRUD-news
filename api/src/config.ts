0cad828d4b49a9db5a0cf07475e2b69c7b9b93e6
Rubendvb <rubenvasquez556@gmail.com>
2021-05-07 20:37:23 -0300

    Criando arquivo config

---
+++ api/src/config.ts
@@ -0,0 +1,6 @@
+export default {
+  MONGO_DATABASE: "newsdb",
+  MONGO_USER: "admin",
+  MONGO_PASSWORD: "admin",
+  MONGO_HOST: "localhost",
+};
--- api/src/database.ts
+++ api/src/database.ts
@@ -1,1 +1,21 @@
-import mongosse from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
+import config from "./config";
+
+(async () => {
+  try {
+    const mongooseOptions: ConnectOptions = {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      //   user: config.MONGO_USER,
+      //   pass: config.MONGO_PASSWORD,
+    };
+
+    const db = await mongoose.connect(
+      `mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,
+      mongooseOptions
+    );
+    console.log("database is connected:", db.connection.name);
+  } catch (error) {
+    console.error(error);
+  }
+})();
