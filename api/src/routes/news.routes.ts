7744c8a22b905de073682007d0b2ab15c014009f
Rubendvb <rubenvasquez556@gmail.com>
2021-05-07 21:17:20 -0300

    Editando news.controller.ts

--- api/src/routes/news.routes.ts
+++ api/src/routes/news.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 const router = Router();
 
-import { getNews } from "./news.controller";
+import * as newsCtrl from "./news.controller";
 
-router.get("/noticias", getNews);
+router.get("/noticias", newsCtrl.getNews);
+
+router.get("/noticias/:id", newsCtrl.getNews);
+
+router.post("/noticias", newsCtrl.createNews);
+
+router.delete("/noticias/:id", newsCtrl.deleteNews);
+
+router.put("/noticias/:id", newsCtrl.updateNews);
 
 export default router;
