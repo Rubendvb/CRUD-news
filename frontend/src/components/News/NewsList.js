63108cbf9143e3209138cb10d6b72e8dc17e1ae1
Rubendvb <rubenvasquez556@gmail.com>
2021-05-08 22:29:16 -0300

    Esruturando componente NewLists

--- frontend/src/components/News/NewsList.js
+++ frontend/src/components/News/NewsList.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as newService from "./NewService";
 
 export const NewsList = () => {
-  return <div>New List</div>;
+  const [news, setNews] = useState([]);
+
+  const loadNews = async () => {
+    const res = await newService.getNews();
+    setNews(res.data);
+  };
+
+  useEffect(() => {
+    loadNews();
+  }, []);
+
+  return (
+    <div>
+      {news.map((i) => (
+        <div>
+          <h1>{i.title}</h1>
+        </div>
+      ))}
+    </div>
+  );
 };
