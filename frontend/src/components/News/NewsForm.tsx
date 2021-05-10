7d17f88d46f7743fb7518ddfbbad065badea962a
Rubendvb <rubenvasquez556@gmail.com>
2021-05-10 18:54:36 -0300

    Finalizando formato do NewsForm

--- frontend/src/components/News/NewsForm.tsx
+++ frontend/src/components/News/NewsForm.tsx
@@ -7,6 +7,38 @@ const NewsForm = () => {
         <div className="card">
           <div className="card-body">
             <h3>Nova Noticia</h3>
+
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Escreve o título da noticia"
+                  className="form-control"
+                  autoFocus
+                />
+              </div>
+
+              <div className="form-group pt-2">
+                <input
+                  type="url"
+                  name="url"
+                  placeholder="https://google.com"
+                  className="form-control"
+                />
+              </div>
+
+              <div className="form-group pt-2">
+                <textarea
+                  name="description"
+                  rows={3}
+                  className="form-control"
+                  placeholder="Descrição do vídeo"
+                ></textarea>
+              </div>
+
+              <button className="btn btn-danger mt-2">Criar Vídeo</button>
+            </form>
           </div>
         </div>
       </div>
