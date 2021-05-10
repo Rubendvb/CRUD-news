f51de62e0c84f72abb54dd1327eda57e07e903fc
Rubendvb <rubenvasquez556@gmail.com>
2021-05-10 01:25:12 -0300

    Erro rodando aplicativo

--- frontend/src/components/News/NewService.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const getNews = async () => {
-  return await axios.get("http://localhost:4000/noticias");
-};
--- frontend/src/components/News/NewsForm.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewsForm = () => {
-  return <div>News Form</div>;
-};
-
-export default NewsForm;
