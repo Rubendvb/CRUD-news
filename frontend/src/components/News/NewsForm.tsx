35f09167a6bb4335f5e47303a779ba0975ad15cc
Rubendvb <rubenvasquez556@gmail.com>
2021-05-10 04:36:26 -0300

    Arquivo NewsList.tsx

---
+++ frontend/src/components/News/NewsForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewsForm = () => {
+  return <div>news form</div>;
+};
+
+export default NewsForm;
---
+++ frontend/src/components/News/NewsList.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+const NewsList = () => {
+  useEffect(() => {
+    console.log("estoooo");
+  }, []);
+  return <div>news list</div>;
+};
+
+export default NewsList;
